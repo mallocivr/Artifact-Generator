006d0ffa9e838b1b661493e9fa2b782f4ab0ac68
Maria Belyi <mashabelyi@berkeley.edu>
2020-02-21 12:19:01 -0800

    subroom artifacts

--- app.py
+++ app.py
@@ -18,15 +18,20 @@ def api_root():
 # 	Returns links of all images uploaded with this service
 #############################################################
 @app.route("/generate",  methods=['POST'])
-@cross_origin()
 def parse_doc():
 	content = request.json
 	
 	for room in content['rooms']:
 		room['artifacts'] = [
-			{'type':'text', 'text':'This is the MOST important artifact.'},
+			{'type':'text', 'text':'{} Artifact. This is the MOST important artifact.'.format(room['name'])},
 			{'type':'image', 'url':'https://static.designboom.com/wp-content/uploads/2018/04/cat-VR-isobar-designboom-600.jpg'}
 		]
+		if 'subRooms' in room:
+			for subroom in room['subRooms']:
+				subroom['artifacts'] = [
+					{'type':'text', 'text':'Subroom Artifact'},
+					{'type':'image', 'url':'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'}
+				]
 	
 
 	return jsonify(content), 200
